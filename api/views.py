bf88649f98b77260df91eb31681ec747d3a9dc0f
Wojciech <w.tolwinski93@gmail.com>
2020-11-04 20:33:52 +0100

    Added docstring to view classes. Added import TransferDetailViewAllowedMethods from permissions.py

--- api/views.py
+++ api/views.py
@@ -15,7 +15,7 @@ from .serializers import (
 )
 from .permissions import (
     CurrencyDetailAllowedMethods, CurrencyListAllowedMethods,
-    ExpensesListAllowedMethods
+    ExpensesListAllowedMethods, TransferDetailViewAllowedMethods
 )
 
 
@@ -42,17 +42,69 @@ class RegisterViewSet(viewsets.ModelViewSet):
             return Response(res)
 
 class CurrencyListView(generics.ListCreateAPIView):
+    """
+    Lists and creates currency objects.
+    Http methods:
+
+    GET - Lists all Currency objects for every user.
+    GET /currencies/
+
+    POST - Creates new Currency. Permitted only for admin user.
+    POST /currencies/
+    Request body:
+    {
+        "currency":"PLN"
+    }
+    """
     serializer_class = CurrencySerializer
     queryset = Currency.objects.all()
     permission_classes = [CurrencyListAllowedMethods]
 
 class CurrencyDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Detail view of currency object.
+    Http methods:
+
+    GET- Retrieves existing Currency object. Permitted for authenticated user.
+    GET /currency/PLN
+
+    PUT- Updates Currency object given in url. Permitted for authenticated
+        user.
+    PUT /currency/EUR/
+    Request body:
+    {
+        "currency":"PLN"
+    }
+
+    DELETE- Deletes currency object given in url. Permitted for superuser.
+    DELETE /currency/PLN
+    """
+
     queryset = Currency.objects.all()
     serializer_class = CurrencySerializer
     lookup_field = 'currency_name'
-    permission_classes = [CurrencyDetailAllowedMethods]
+    permission_classes = [CurrencyDetailAllowedMethods,IsAuthenticated]
 
 class ExpensesListView(generics.ListCreateAPIView):
+    """
+    Lists and creates expense objects
+    Http methods:
+
+    GET - Lists all Expenses objects owned by user. For superuser shows
+        everything.
+    GET /expenses/
+
+    POST - Creates new expense. Permitted only for admin user.
+    POST /expenses/
+    Request body:
+    {
+        'currency':'PLN',
+        'total_amount':'1000',
+        'vat':'false',
+        'owner':'2'
+    }
+    """
+
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
     lookup_field = 'id'
@@ -64,7 +116,19 @@ class ExpensesListView(generics.ListCreateAPIView):
         return Expense.objects.filter(owner=self.request.user)
 
 
-class ExpenseDetailView(generics.RetrieveUpdateDestroyAPIView):
+class ExpenseDetailView(generics.RetrieveDestroyAPIView):
+    """
+    Detail view of expense objects.
+    All methods permitted only for  admin user.
+
+    Http methods:
+    GET- Retrieves existing Expenses object given in url.
+    GET /expense/1
+
+    DELETE- Deletes expense object given in url.
+    DELETE /expense/1
+    """
+
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
     lookup_field = 'id'
@@ -77,6 +141,27 @@ class ExpenseDetailView(generics.RetrieveUpdateDestroyAPIView):
 
 
 class TransfersListView(generics.ListCreateAPIView):
+    """
+    Lists and creates transfer objects.
+
+    Http methods:
+
+    GET - Lists all Transfer objects owned by user. For superuser shows
+        everything.
+    GET /transfers/
+
+    POST - Creates new transfer. Permitted only for autheticated users.
+    POST /transfers/
+    Request body:
+    {
+        'currency':'PLN',
+        'is_vat':'false'
+        'netto':'1000',
+        'vat':'0',
+        'expense':'1',
+        'owner':'2'
+    }
+    """
     queryset = Transfer.objects.all()
     serializer_class = TransferSerializer
     lookup_field = 'id'
@@ -94,10 +179,30 @@ class TransfersListView(generics.ListCreateAPIView):
 
 
 class TransferDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Returns detail view for transfer object.
+
+    Http methods:
+
+    GET- Retrieves existing Transfer object. Permitted for authenticated user.
+    GET /transfer/<transfer id>
+
+    PUT- Updates Transfer object given in url. Permitted for admin users.
+    PUT /transfer/<transfer id>
+    Request body:
+    {
+        "is_settled":"true"
+    }
+
+    DELETE- Deletes Transfer object given in url. Permitted for authorized
+        users.
+    DELETE /transfer/<transfer id>
+    """
+
     queryset = Transfer.objects.all()
     serializer_class = TransferSerializer
     lookup_field = 'id'
-    permission_classes = [IsAuthenticated]
+    permission_classes = [TransferDetailViewAllowedMethods, IsAuthenticated]
 
     def get_serializer_class(self):
         if self.request.user.is_superuser:
@@ -111,6 +216,11 @@ class TransferDetailView(generics.RetrieveUpdateDestroyAPIView):
 
 
 class StatisticsListView(APIView):
+    """
+    List of generated statistics values.
+
+    GET /statistics/
+    """
     permission_classes = [IsAuthenticated]
     def get(self, request, format=None):
         sum_settled_name = 'Suma wszystkich wydatków rozliczonych'
